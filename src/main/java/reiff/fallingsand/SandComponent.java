9d229f1e3e09ce907ec7319bd2f7f9251428e0f3
Sheila Reiff <sheilareiff@Sheilas-MacBook-Air-7.local>
2024-03-20 10:19:11 -0400

    hw gui fixed

--- src/main/java/reiff/fallingsand/SandComponent.java
+++ src/main/java/reiff/fallingsand/SandComponent.java
@@ -6,6 +6,7 @@ import java.awt.*;
 public class SandComponent extends JComponent {
 
     private final Sand sand;
+    private final Color standardSandColor = Color.decode("#C2B280");
 
     public SandComponent(Sand sand) {
         this.sand = sand;
@@ -15,7 +16,7 @@ public class SandComponent extends JComponent {
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.setColor(Color.decode("#C2B280"));
+        g.setColor(standardSandColor);
         for (int y = 0; y < sand.getHeight(); y++) {
             for (int x = 0; x < sand.getWidth(); x++) {
                 if (sand.get(x, y) == 1) {
